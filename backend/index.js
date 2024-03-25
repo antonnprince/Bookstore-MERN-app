231feac498f571806b05a6eded351084fd1ac1d0
antonnprince <antonprince95@gmail.com>
2024-03-25 21:58:33 +0530

    express setup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@ import {PORT} from "./config.js";
 
 const app = express();
 
+//sets up the route handler for root URL
+//when GET req made, callback function will be executed
+app.get('/',(request,response)=>{
+    console.log(request)
+    return response.status(234).send('Welcome to MERN')
+});
+
 app.listen(PORT, ()=>{
     console.log(`App is listening to port ${PORT}`)
 })
