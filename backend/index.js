93c409972778230086a2f376f9b4bbcca13ea54e
antonnprince <antonprince95@gmail.com>
2024-03-28 09:09:24 +0530

    axios init

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,15 @@ app.use(express.json()) //to use json format data
 //sets up the route handler for root URL
 //when GET req made, callback function will be executed
 
+app.use(cors())
 
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods:['GET','POST','PUT','DELETE'],
-        allowedHeaders: ['Content-Type']
-    })
-)
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods:['GET','POST','PUT','DELETE'],
+//         allowedHeaders: ['Content-Type']
+//     })
+// )
 
 
 app.get('/',(request,response)=>{
